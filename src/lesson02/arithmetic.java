19188ef04a4bc1c6aae2839417d58597c5fef943
Arina Yu <arinau70@gmail.com>
2025-11-12 16:33:10 +0400

    update gitignore

---
+++ src/lesson02/arithmetic.java
@@ -0,0 +1,15 @@
+package lesson02;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 3;
+
+        System.out.println("a = 20, b = 3");
+        System.out.println("Сумма: " + (a+b));
+        System.out.println("Разность: " + (a-b));
+        System.out.println("Произедение: " + (a*b));
+        System.out.println("Частное: " + (a/b));
+        System.out.println("Остаток от деления: " + (a%b));
+    }
+}
